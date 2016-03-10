94a41dd73dc18dc64022a6f5080943c2f19298c0
sparlant <sandra.parlant@gmail.com>
2016-03-10 16:38:53 +0100

    FIX npe sans authent

--- liquid-democracy-api/src/main/java/com/zenika/liquid/democracy/api/util/AuthenticationUtil.java
+++ liquid-democracy-api/src/main/java/com/zenika/liquid/democracy/api/util/AuthenticationUtil.java
@@ -8,13 +8,17 @@ import org.springframework.security.oauth2.provider.OAuth2Authentication;
 public class AuthenticationUtil {
 
 	public static String getUserIdentifiant(OAuth2Authentication authentication) {
-		Map<String, Object> details = (Map<String, Object>) authentication.getUserAuthentication().getDetails();
+		if (authentication != null) {
+			Map<String, Object> details = (Map<String, Object>) authentication.getUserAuthentication().getDetails();
 
-		JSONArray emailArray = new JSONArray(details.get("emails").toString());
+			JSONArray emailArray = new JSONArray(details.get("emails").toString());
 
-		Map<String, Object> zenikaEmail = (Map<String, Object>) emailArray.get(0);
+			Map<String, Object> zenikaEmail = (Map<String, Object>) emailArray.get(0);
 
-		return zenikaEmail.get("value").toString();
+			return zenikaEmail.get("value").toString();
+		} else {
+			return "";
+		}
 	}
 
 }
