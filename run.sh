3e8732c535bc815673f66a812dd14bbc53e95131
sparlant <sandra.parlant@zenika.com>
2016-04-14 15:22:50 +0200

    install du model dans le script d'install local

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 echo "building liquid-democracy-api" 
 docker rm -f $(docker ps -aq)
+docker run -ti --rm -v ~/.m2:/root/.m2 -v $(pwd)/liquid-democracy-model:/usr/src/app -w /usr/src/app -v /var/run/docker.sock:/var/run/docker.sock maven:3.2-jdk-8 mvn clean install
 docker run -ti --rm -v ~/.m2:/root/.m2 -v $(pwd)/liquid-democracy-api:/usr/src/app -w /usr/src/app -v /var/run/docker.sock:/var/run/docker.sock maven:3.2-jdk-8 mvn clean package docker:build -DskipTests
 
 cd liquid-democracy-api
