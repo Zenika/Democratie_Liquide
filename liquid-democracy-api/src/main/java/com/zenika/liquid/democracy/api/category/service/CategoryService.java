ce873377d452e922e68ed0f6e91c959b2c3e00f7
Valentin Bailleul <vlt.bailleul@gmail.com>
2016-10-27 10:40:49 +0200

    Check if a category title exists before creating it

--- liquid-democracy-api/src/main/java/com/zenika/liquid/democracy/api/category/service/CategoryService.java
+++ liquid-democracy-api/src/main/java/com/zenika/liquid/democracy/api/category/service/CategoryService.java
@@ -2,6 +2,7 @@ package com.zenika.liquid.democracy.api.category.service;
 
 import java.util.List;
 
+import com.zenika.liquid.democracy.api.category.exception.ExistingCategoryException;
 import org.springframework.stereotype.Service;
 
 import com.zenika.liquid.democracy.api.category.exception.MalformedCategoryException;
@@ -11,7 +12,7 @@ import com.zenika.liquid.democracy.model.Category;
 @Service
 public interface CategoryService {
 
-	public Category addCategory(Category c) throws MalformedCategoryException;
+	public Category addCategory(Category c) throws MalformedCategoryException, ExistingCategoryException;
 
 	public List<Category> getCategories();
 
--- liquid-democracy-api/src/main/java/com/zenika/liquid/democracy/api/category/service/impl/CategoryServiceImpl.java
+++ liquid-democracy-api/src/main/java/com/zenika/liquid/democracy/api/category/service/impl/CategoryServiceImpl.java
@@ -3,6 +3,7 @@ package com.zenika.liquid.democracy.api.category.service.impl;
 import java.util.List;
 import java.util.Optional;
 
+import com.zenika.liquid.democracy.api.category.exception.ExistingCategoryException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,11 +20,22 @@ public class CategoryServiceImpl implements CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 
-	public Category addCategory(Category c) throws MalformedCategoryException {
+	public Category addCategory(Category newCategory) throws MalformedCategoryException, ExistingCategoryException {
 
-		CategoryUtil.checkCategory(c);
+		// check category not blank
+		CategoryUtil.checkCategory(newCategory);
 
-		return categoryRepository.save(c);
+		// trim and lowerCase title
+		newCategory.setTitle(newCategory.getTitle().toLowerCase().trim());
+
+		// find duplicates
+		Optional<Category> c = categoryRepository.findCategoryByTitle(newCategory.getTitle());
+		if (c.isPresent()) {
+			throw new ExistingCategoryException();
+		}
+
+
+		return categoryRepository.save(newCategory);
 	}
 
 	public List<Category> getCategories() {
